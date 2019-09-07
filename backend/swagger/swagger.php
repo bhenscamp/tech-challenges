3e85b5889b2edcbc389fead748047f141271851d
Benjamin Henseler-Campana <benjamin@macbook-pro-de-benjamin.home>
2019-09-07 17:17:12 +0200

    Init add swagger-php as dependency install swagger-ui for easy check create swagger.php definition file add DS_Store to gitignore as working on mac

---
+++ backend/swagger/swagger.php
@@ -0,0 +1,11 @@
+<?php
+
+use OpenApi\Annotations as OA;
+
+/** 
+ * @OA\Info(title="IDW backend interview", version="0.1")
+ * @OA\Server(
+ *      url="http://localhost:8080/api/v1",
+ *      description="api for IDW backend interview"
+ * )
+ */
